9793acef1bed4ae13b672067449a2c50e3a45a2a
jmsmith1018 <jmsmith1018@gmail.com>
2021-11-11 11:04:42 -0700

    Add classes for both of the player types because of how different their functionality is. Both inherit from Player

---
+++ code_breaker.rb
@@ -0,0 +1,15 @@
+# frozen_string_literal: true
+
+require_relative 'player'
+
+# CodeBreaker class that inherits from Player and holds logic for the player
+# currently guessing the secret code.
+class CodeBreaker < Player
+  def make_guess
+    p 'Input four numbers between 1 and 6 to make your guess'
+    gets.chomp.downcase.delete(' ').split(' ')
+  end
+end
+
+player = CodeBreaker.new('player_one')
+p player.make_guess
