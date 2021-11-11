57aabc2fc9a7169f00317c6db3379ca4b0b1d491
jmsmith1018 <jmsmith1018@gmail.com>
2021-11-11 15:21:44 -0700

    Fix method for checking guess-code against the board code

--- lib/board.rb
+++ lib/board.rb
@@ -10,12 +10,14 @@ class Board
 
   def guess_response(guess)
     # return hash with perfect matches, imperfect matches, and empties
-    matches = guess.map.with_index { |element, index| element == @code[index] }.count(true)
-    imp_matches = guess.count { |element| @code.include?(element) } - matches
+    matches = guess.map.with_index { |element, index| element == @code[index] || element }
+    match_count = matches.count(true)
+    code_clone = @code.split('').reject.with_index { |element, index| element == guess[index] }
+    imp_matches = matches.count { |element| code_clone.include?(element) }
     {
-      matches: matches,
+      matches: match_count,
       imp_matches: imp_matches,
-      none: (4 - (imp_matches + matches))
+      none: 4 - (imp_matches + match_count)
     }
   end
 
@@ -27,7 +29,7 @@ class Board
 end
 
 # test
-my_code = %w[1 2 3 4]
-my_board = Board.new(my_code)
-new_guess = %w[1 1 2 2]
-p my_board.guess_response(new_guess)
+# my_code = %w[1 2 3 4]
+# my_board = Board.new(my_code)
+# new_guess = %w[1 3 3 4]
+# p my_board.guess_response(new_guess)
