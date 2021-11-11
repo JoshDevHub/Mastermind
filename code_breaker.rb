4006d79789b97365e5a4f816fe9bd128ae3894c9
jmsmith1018 <jmsmith1018@gmail.com>
2021-11-11 13:01:30 -0700

    Improve logic for accepting and sanitizing user code guesses

--- code_breaker.rb
+++ code_breaker.rb
@@ -7,10 +7,32 @@ require_relative 'player'
 class CodeBreaker < Player
   def make_guess
     p 'Input four numbers between 1 and 6 to make your guess'
+    input = gets_user_input
+    return input if valid_input?(input)
+
+    sanitize_user_input
+  end
+
+  def gets_user_input
     gets.chomp.downcase.delete(' ').split('')
+  end
+
+  def sanitize_user_input
+    is_valid = false
+    correct_input = ''
+    until is_valid
+      p 'Invalid input. Please only guess four numbers and only use digits between 1 and 6'
+      correct_input = gets_user_input
+      is_valid = valid_input?(correct_input)
+    end
+    correct_input
+  end
+
+  def valid_input?(code)
+    code.length == 4 && code.all? { |number| '123456'.include?(number) }
   end
 end
 
 # test
-# player = CodeBreaker.new('player_one')
-# p player.verify_code(%w[1 2 3 7])
+player = CodeBreaker.new('player_one')
+p player.make_guess
