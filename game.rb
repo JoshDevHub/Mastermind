93d72246d2cece26face8531e3ee6971ae516aaf
jmsmith1018 <jmsmith1018@gmail.com>
2021-11-11 11:14:02 -0700

    Develop Game class to hold logic for running the game

--- game.rb
+++ game.rb
@@ -1,6 +1,23 @@
 # frozen_string_literal: true
 
+require_relative 'code_maker'
+require_relative 'code_breaker'
+require_relative 'board'
+
 # Game Class that holds logic for progressing & deciding the game
 class Game
+  attr_reader :player_one, :player_two
 
+  def initialize(player_one, player_two)
+    @player_one = CodeMaker.new(player_one)
+    @player_two = CodeBreaker.new(player_two)
+  end
+
+  def create_board(code = @player_one.make_code)
+    Board.new(code)
+  end
 end
+
+# test
+# my_game = Game.new('computer', 'Josh')
+# p my_game.player_one
