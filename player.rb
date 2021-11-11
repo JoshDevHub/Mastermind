7d048517b68a394236490cd8e95ba33e1489b2c0
jmsmith1018 <jmsmith1018@gmail.com>
2021-11-11 10:24:19 -0700

    Create Player class and logic for keeping score

--- player.rb
+++ player.rb
@@ -2,5 +2,20 @@
 
 # Player Class for holding logic related to players
 class Player
+  attr_accessor :score
+  attr_reader :player
 
+  def initialize(player)
+    @name = player
+    @score = 0
+  end
+
+  def increment_score
+    self.score += 1
+  end
 end
+
+# test
+player_one = Player.new('player_one')
+player_one.increment_score
+p player_one.score
