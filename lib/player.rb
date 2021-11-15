25d2264a313bf9b27e55cf033eb66e53c9bf91d0
jmsmith1018 <jmsmith1018@gmail.com>
2021-11-15 15:10:53 -0700

    Move methods for accepting code input into the Player class for better organization

--- lib/player.rb
+++ lib/player.rb
@@ -21,10 +21,36 @@ class Player
     metaclass = class << self; self end
     mod.instance_methods.each { |method_name| metaclass.class_eval { undef_method(method_name.to_sym) } }
   end
+
+  def take_code_input
+    input = gets_user_input
+    return input if valid_input?(input)
+
+    sanitize_user_input
+  end
+
+  def gets_user_input
+    gets.chomp.downcase.delete(' ').split('')
+  end
+
+  def sanitize_user_input
+    is_valid = false
+    correct_input = ''
+    until is_valid
+      puts 'Invalid input. Please only use four numbers and only use digits between 1 and 6'
+      correct_input = gets_user_input
+      is_valid = valid_input?(correct_input)
+    end
+    correct_input
+  end
+
+  def valid_input?(code)
+    code.length == 4 && code.all? { |number| '123456'.include?(number) }
+  end
 end
 
 # test
 # my_player = Player.new('Josh')
 # my_player.extend CodeMaster
-# my_player.test_method
-# p my_player.methods.include?
+# my_code = my_player.make_code
+# p my_code
