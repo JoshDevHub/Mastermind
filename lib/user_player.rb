68709a44ac462b16ac9d2803cf522dd0bef7b317
jmsmith1018 <jmsmith1018@gmail.com>
2021-11-16 13:07:56 -0700

    Add in methods based around getting user input and refactor them to fit with the current class dynamic and game loop

--- lib/user_player.rb
+++ lib/user_player.rb
@@ -5,17 +5,17 @@ require_relative 'player'
 # UserPlayer class that holds the logic for getting user inputs into the game
 class UserPlayer < Player
   def take_code_input
-    input = gets_user_input
+    input = gets_user_code_input
     return input if valid_input?(input)
 
     sanitize_user_input
   end
 
-  def gets_user_input
+  def gets_user_code_input
     gets.chomp.downcase.delete(' ').split('')
   end
 
-  def sanitize_user_input
+  def sanitize_user_code_input
     is_valid = false
     correct_input = ''
     until is_valid
@@ -28,5 +28,27 @@ class UserPlayer < Player
 
   def valid_input?(code)
     code.length == 4 && code.all? { |number| '123456'.include?(number) }
+  end
+
+  def player_order
+    order = gets.chomp.downcase
+    sanitize_yes_no_input(order)
+  end
+
+  def valid_yes_no_input?(input)
+    %w[y n].include?(input)
+  end
+
+  def sanitize_yes_no_input(input)
+    return input if valid_yes_no_input?(input)
+
+    correct_input = ''
+    is_valid = false
+    until is_valid
+      puts 'Invalid input. Please use Y or N to respond'
+      correct_input = gets.chomp.downcase
+      is_valid = valid_yes_no_input?(correct_input)
+    end
+    correct_input
   end
 end
