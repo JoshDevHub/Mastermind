621e111972bb161091c072d6bbf54f6e8eb57452
jmsmith1018 <jmsmith1018@gmail.com>
2021-11-16 14:11:25 -0700

    Full refactor of the class logic to account for other changes in the helper classes

--- lib/game.rb
+++ lib/game.rb
@@ -1,5 +1,6 @@
 # frozen_string_literal: true
 
+require 'pry-byebug'
 require_relative 'user_player'
 require_relative 'computer_player'
 require_relative 'board'
@@ -8,68 +9,53 @@ require_relative 'display'
 # Game Class that holds logic for progressing & deciding the game
 class Game
   attr_accessor :user_player, :computer_player
-  attr_reader :game_over, :game_display
+  attr_reader :game_over, :game_display, :round_number
 
   def initialize(player_name)
     @user_player = UserPlayer.new(player_name)
     @computer_player = ComputerPlayer.new('Computer')
-    @round_number = 0
-    @game_over = false
+    @round_number = 1
+    @user_first = false
     @game_display = Display.new
   end
 
-  # TODO: Move following 3 methods to user_player
-  def player_order
+  def round_computer_master
+    board_code = @computer_player.make_code
+    new_board = create_board(board_code)
+    loop do
+      # binding.pry
+      @game_display.code_query(@round_number)
+      guess = @user_player.take_code_input
+      response_hash = new_board.guess_response(guess)
+      @game_display.give_round_feedback(response_hash)
+      increment_round
+      break if check_win_condition(board_code, guess)
+    end
+  end
+
+  def round_user_master
+    @game_display.create_master_code
+    board_code = @user_player.take_code_input
+    new_board = create_board(board_code)
+    @computer_player.solve_code
+  end
+
+  def order
     @game_display.order_query
-    order = gets.chomp.downcase
-    sanitize_yes_no_input(order)
-  end
-
-  def valid_yes_no_input?(input)
-    %w[y n].include?(input)
-  end
-
-  def sanitize_yes_no_input(input)
-    return input if valid_yes_no_input?(input)
-
-    correct_input = ''
-    is_valid = false
-    until is_valid
-      @game_display.alt_order_query
-      correct_input = gets.chomp.downcase
-      is_valid = valid_yes_no_input?(correct_input)
-    end
-    correct_input
-  end
-
-  def control_player_order(order)
-    if order == 'y'
-      computer_breaker_user_master
-    else
-      computer_master_user_breaker
-    end
-  end
-
-  def computer_master_user_breaker
-    @player_one.extend CodeBreaker
-    @player_two.extend CodeMaster
-  end
-
-  def computer_breaker_user_master
-    @player_one.extend CodeMaster
-    @player_two.extend CodeBreaker
+    order_response = @user_player.player_order
+    @user_first = true if order_response == 'y'
   end
 
   def increment_round
     @round_number += 1
   end
 
-  def create_board(code = @player_two.make_code)
+  def create_board(code)
     Board.new(code)
   end
 
   def check_win_condition(board_code, code_guess)
-    @game_over = true if @round_number > 12 || board_code == code_guess
+    @round_number > 12 || board_code == code_guess
   end
 
   def start_game
@@ -78,22 +64,23 @@ class Game
     control_player_order(player_choice)
   end
 
-  def play_round(board)
-    @game_display.code_query
-    if @player_one.methods.include? :make_code
-      puts 'Placeholder logic'
+  def play_game
+    if @user_first
+      round_user_master
+      between_games(user_player, computer_player)
+      round_computer_master
+      # TODO: End game logic
     else
-      play_round_user_breaker(board)
+      round_computer_master
+      between_games(computer_player, user_player)
+      round_user_master
+      # TODO: End game logic
     end
   end
 
-  def play_round_user_breaker(board)
-    code_guess = @player_one.take_code_input
-    response = board.guess_response(code_guess)
-    @game_display.give_round_feedback(response)
-    check_win_condition(board.code, code_guess)
-    player_one.increment_score
-    increment_round
+  def between_games(master, breaker)
+    master.score = @round_number
+    @game_display.update_between_games(master, breaker)
   end
 
   def end_game
@@ -103,10 +90,7 @@ class Game
 end
 
 # test
-# my_game = Game.new('Computer', 'Josh')
-# # my_game.check_win_condition(13, '1234', '1243')
+my_game = Game.new('Josh')
+my_game.order
+my_game.play_game
 # p my_game.player_order
-
-# TODO: full refactor into the current player dynamic
-# TODO: have the game fully functional with the placeholder of the computer
-#       player guessing the user's code when computer is Breaker.
