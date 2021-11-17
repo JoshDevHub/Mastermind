a548d635c409b5bbb809863fe3ccede0680179b4
jmsmith1018 <jmsmith1018@gmail.com>
2021-11-17 09:56:17 -0700

    Add method for creating the full possibility space of codes for the computer to guess

--- lib/computer_player.rb
+++ lib/computer_player.rb
@@ -6,6 +6,18 @@ require_relative 'player'
 # the master code and for the guessing algorithm it uses when the player is Code
 # Master
 class ComputerPlayer < Player
+  attr_reader :possibility_space
+
+  def initialize(name)
+    super(name)
+    @code_array = *(1..6)
+    @possibility_space = []
+  end
+
+  def populate_possibility_space
+    @code_array.repeated_permutation(6) { |permutation| @possibility_space << permutation }
+  end
+
   def make_code
     code = []
     4.times do
@@ -19,3 +31,9 @@ class ComputerPlayer < Player
     puts 'This is a placeholder'
   end
 end
+
+# test
+my_ai = ComputerPlayer.new('Computer')
+my_ai.solve_code
+my_ai.populate_possibility_space
+p my_ai.possibility_space
