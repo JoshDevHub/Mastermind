0d69ddeeb9e59f7f20ac75130376d75ff9d6dd19
jmsmith1018 <jmsmith1018@gmail.com>
2021-11-23 19:44:18 -0700

    Remove unused name from initializer

--- lib/player.rb
+++ lib/player.rb
@@ -2,12 +2,9 @@
 
 # Player Class for holding logic related to players
 class Player
-  # TODO: Consider name and its usefulness?
-  attr_accessor :name
   attr_reader :score
 
-  def initialize(name)
-    @name = name
+  def initialize
     @score = 0
   end
 
